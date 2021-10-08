99cb28ebc07c896b4d67d5f46ca2462034c0534e
Cierra Smith <cierra.r.smith3@gmail.com>
2021-10-08 10:43:49 -0500

    inital commit

---
+++ src/main/java/com/thyme/mythyme/MyThymeApplication.java
@@ -0,0 +1,13 @@
+package com.thyme.mythyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyThymeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyThymeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thyme/mythyme/MyThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thyme.mythyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThymeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
