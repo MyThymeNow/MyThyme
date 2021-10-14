b9b0a70073b58da2d6ec5b7388f5cee313986260
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-10-14 12:11:56 -0500

    added integration to header. issues loading pages

--- src/main/java/com/thyme/mythyme/controllers/UserController.java
+++ src/main/java/com/thyme/mythyme/controllers/UserController.java
@@ -11,25 +11,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    private Users users;
-    private PasswordEncoder passwordEncoder;
+    private final Users userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(Users users, PasswordEncoder passwordEncoder) {
-        this.users = users;
+    public UserController(Users userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
     }
+
 
     @GetMapping("/sign-up")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
-        return "users/sign-up";
+        return "user/sign-up";
     }
 
     @PostMapping("/sign-up")
     public String saveUser(@ModelAttribute User user){
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
-        users.save(user);
+        userDao.save(user);
         return "redirect:/login";
     }
 }
