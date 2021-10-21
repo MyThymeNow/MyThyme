85585184597622868b18b7e0b471524792093f0a
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-10-21 15:21:56 -0500

    constructor, getters and setters for sending messages

--- src/main/java/com/thyme/mythyme/controllers/AuthenticationController.java
+++ src/main/java/com/thyme/mythyme/controllers/AuthenticationController.java
@@ -34,9 +34,7 @@ public class AuthenticationController {
             return "redirect:/profile"; // /user/location not working
 
 
-            return "redirect:/user/view-profile"; // /user/location not working
 
-            return "redirect:user/view-profile";
 
 
         }
---
+++ src/main/java/com/thyme/mythyme/repository/Messages.java
@@ -0,0 +1,9 @@
+package com.thyme.mythyme.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.*;
+
+public interface Messages extends JpaRepository<Messages, String> {
+
+}
