47dffe7c6ebf2be74fc1fd834f98fdb8f325b69e
Kamrun Foster <kamrun.foster4@gmail.com>
2021-10-20 23:29:38 -0500

    working on controller updating

--- src/main/java/com/thyme/mythyme/controllers/ListController.java
+++ src/main/java/com/thyme/mythyme/controllers/ListController.java
@@ -1,24 +1,31 @@
 package com.thyme.mythyme.controllers;
 
+import com.thyme.mythyme.models.User;
+import com.thyme.mythyme.repository.UserRepository;
+import lombok.Getter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ListController {
+    private final UserRepository userList;
+
+    public ListController(UserRepository userList){
+        this.userList = userList;
+    }
 
     @GetMapping("/coupons")
     public String groceryCoupons(){
         return "user/coupons";
     }
-    }
-//}
-
-class HelloController {
-
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring!";
-    }
 }
+
+
+
+
+
