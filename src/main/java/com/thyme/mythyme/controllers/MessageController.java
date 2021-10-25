b640774a87e7b9aed6d950fe74a64c1007f6813c
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-10-25 11:16:47 -0500

    rebuilding message controller following Aarion's setup

--- src/main/java/com/thyme/mythyme/controllers/MessageController.java
+++ src/main/java/com/thyme/mythyme/controllers/MessageController.java
@@ -1,21 +1,53 @@
 package com.thyme.mythyme.controllers;
 
 import com.thyme.mythyme.models.User;
+import com.thyme.mythyme.repository.Messages;
+import com.thyme.mythyme.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
 
 @Controller
 public class MessageController {
 
-    @GetMapping("/messages")
-    public String messages(Model model){
+    private MessagesRepository messageDao;
+
+    private final UserRepository userDao;
+
+    public MessageController(UserRepository userDao) {
+
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/messages/{id}")
+    public String messages(Model model, @PathVariable long id) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.getById(loggedInUser.getId());
+        model.addAttribute("loggedinuser", user);
 
-        model.addAttribute("user", user);
+        model.addAttribute("message", new Message());
 
+        List<Message> messageList = user.getReceivedMessages();
+        List<Message> sentMessages = user.getSentMessages();
 
-        return "user/messages";
+        for (Message message : listOfMessagesSent) {
+            listOfMessages.add(message);
+        }
+
+        Collections.sort(listOfMessages);
+
+        User receivingUser = userDao.getById(id);
+
+        model.addAttribute("otheruser", receivingUserUser);
+        model.addAttribute("messages", listOfMessages);
+
+        return "/messages/message";
+
     }
 }
--- src/main/java/com/thyme/mythyme/repository/Messages.java
+++ src/main/java/com/thyme/mythyme/repository/Messages.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
-public interface Messages extends JpaRepository<Messages, String> {
+public interface Messages extends JpaRepository<Messages, Long> {
 
 }
