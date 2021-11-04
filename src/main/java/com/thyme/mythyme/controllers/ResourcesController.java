6dae298bc2fde4d13fbb733780ac8138ce2e1787
Kamrun Foster <kamrun.foster4@gmail.com>
2021-11-04 15:49:28 -0500

    commented out resource controller

--- src/main/java/com/thyme/mythyme/controllers/ResourcesController.java
+++ src/main/java/com/thyme/mythyme/controllers/ResourcesController.java
@@ -1,25 +1,25 @@
-package com.thyme.mythyme.controllers;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class ResourcesController {
-
-    @Value("${MAPBOXAPIKEY}")
-    private String mapboxKey;
-
-    @Value("${COCKTAILDBAPIKEY}")
-    private String cocktailDBKey;
-
-    @Value("${SPOONACULARAPIKEY}")
-    private String spoonacularKey;
-
-    @GetMapping(path = "/keys.js", produces = "application/javascript")
-    @ResponseBody
-    public String getAPIKeys() {
-        return "const mapboxKey='"+mapboxKey+"'\nconst cocktailDBKey='"+cocktailDBKey+"'\nconst spoonacularKey='"+spoonacularKey+"'";
-    }
-}
+//package com.thyme.mythyme.controllers;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class ResourcesController {
+//
+//    @Value("${MAPBOXAPIKEY}")
+//    private String mapboxKey;
+//
+//    @Value("${COCKTAILDBAPIKEY}")
+//    private String cocktailDBKey;
+//
+//    @Value("${SPOONACULARAPIKEY}")
+//    private String spoonacularKey;
+//
+//    @GetMapping(path = "/keys.js", produces = "application/javascript")
+//    @ResponseBody
+//    public String getAPIKeys() {
+//        return "const mapboxKey='"+mapboxKey+"'\nconst cocktailDBKey='"+cocktailDBKey+"'\nconst spoonacularKey='"+spoonacularKey+"'";
+//    }
+//}
