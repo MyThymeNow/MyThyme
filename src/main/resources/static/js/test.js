bb72a642ce017c56ccecb6bff1cfd78b5f6897b2
Cierra Smith <cierra.r.smith3@gmail.com>
2021-11-05 16:00:57 -0500

    api testing added

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,45 @@
+// console.log(spoonacularKey);
+
+// let requestString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + spoonacularKey + "&ingredients=apples,+flour,+sugar&number=2";
+
+let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
+
+
+$.ajax({
+    url: requestString,
+    type: "GET"
+}).done(function(data) {
+    console.log(data);
+
+    $("#test-col").html("");
+    data.results.forEach(function (item, index) {
+        if (index < 5) {
+            console.log(item);
+
+            var breadTest = `
+                    <div class="column" id="test-col">
+                        <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+
+                            <img src="https://spoonacular.com/cdn/ingredients_100x100/${item.image}">
+                            <li class="list-group-item">Name: ${item.name}</li>
+                            <li class="list-group-item">Id: ${item.id}</li>
+                        </ul>
+                    </div>`
+            $("#test-col").append(breadTest);
+        }
+    })
+
+});
+
+
+
+$.ajax({
+    url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
+    type: "GET"
+}).done(function(data) {
+    console.log(data);
+});
+
+
+
