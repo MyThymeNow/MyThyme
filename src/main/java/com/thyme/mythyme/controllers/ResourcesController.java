e43003f56e189ed140199696d5f346a55372cce3
Kamrun Foster <kamrun.foster4@gmail.com>
2021-11-05 21:29:42 -0500

    update key

--- src/main/java/com/thyme/mythyme/controllers/ResourcesController.java
+++ src/main/java/com/thyme/mythyme/controllers/ResourcesController.java
@@ -1,33 +1,25 @@
-//package com.thyme.mythyme.controllers;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//public class ResourcesController {
-//
-<<<<<<< HEAD
-//    @Value("${MAPBOXAPIKEY}")
-=======
-//    @Value("")
->>>>>>> a89deec7667d6b57511f11d6fcbd78fbef2e86af
-//    private String mapboxKey;
-//
-//    @Value("${COCKTAILDBAPIKEY}")
-//    private String cocktailDBKey;
-//
-<<<<<<< HEAD
-//    @Value("${SPOONACULARAPIKEY}")
-=======
-//    @Value("")
->>>>>>> a89deec7667d6b57511f11d6fcbd78fbef2e86af
-//    private String spoonacularKey;
-//
-//    @GetMapping(path = "/keys.js", produces = "application/javascript")
-//    @ResponseBody
-//    public String getAPIKeys() {
-//        return "const mapboxKey='"+mapboxKey+"'\nconst cocktailDBKey='"+cocktailDBKey+"'\nconst spoonacularKey='"+spoonacularKey+"'";
-//    }
-//}
+package com.thyme.mythyme.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ResourcesController {
+
+    @Value("${MAPBOXAPIKEY}")
+    private String mapboxKey;
+
+    @Value("${COCKTAILDBAPIKEY}")
+    private String cocktailDBKey;
+
+    @Value("${SPOONACULARAPIKEY}")
+    private String spoonacularKey;
+
+    @GetMapping(path = "/keys.js", produces = "application/javascript")
+    @ResponseBody
+    public String getAPIKeys() {
+        return "const mapboxKey='"+mapboxKey+"'\nconst cocktailDBKey='"+cocktailDBKey+"'\nconst spoonacularKey='"+spoonacularKey+"'";
+    }
+}
