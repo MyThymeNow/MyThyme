4039fd0017efb66e7aaca70238aa065424d9ec2c
Cierra Smith <cierra.r.smith3@gmail.com>
2021-11-08 17:25:26 -0600

    API layout for landing page and styling

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -2,7 +2,9 @@
 // let requestString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + spoonacularKey + "&ingredients=apples,+flour,+sugar&number=2";
 
 //Spoonacular
-let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
+// let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
+let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
+// let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
 
 $.ajax({
     url: requestString,
@@ -10,33 +12,52 @@ $.ajax({
 }).done(function(data) {
     console.log(data);
 
-    $("#test-col").html("");
-    data.results.forEach(function (item, index) {
-        if (index < 5) {
-            console.log(item);
+    let html = "";
+    for (let i = 0; i < 3; i++) {
+        html += `
+                  <div class="card border-0" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${data.recipes[i].title}</h5>
+                      <img src="${data.recipes[i].image}">
+                      <p class="card-text">${data.recipes[i].summary}</p>
+                      <a href="#" class="btn btn-primary">View recipe</a>
 
-            var breadTest = `
-                    <div class="column" id="test-col">
-                        <div class="card" style="width: 18rem;">
-                        <ul class="list-group list-group-flush">
+                    </div>
+                  </div>`;
+    }
+    $("#test-col").html(html);
 
-                            <img src="https://spoonacular.com/cdn/ingredients_100x100/${item.image}">
-                            <li class="list-group-item">Name: ${item.name}</li>
-                            <li class="list-group-item">Id: ${item.id}</li>
-                        </ul>
-                    </div>`
-            $("#test-col").append(breadTest);
-        }
-    })
+
+
+
+
+    // $("#test-col").html("");
+    // data.results.forEach(function (item, index) {
+    //     if (index < 3) {
+    //         console.log(item);
+    //
+    //         var breadTest = `
+    //                 <div class="column" id="test-col">
+    //                     <div class="card" style="width: 18rem;">
+    //                     <ul class="list-group list-group-flush">
+    //
+    //                         <img src="https://spoonacular.com/cdn/ingredients_100x100/${item.image}">
+    //                         <li class="list-group-item">Name: ${item.name}</li>
+    //
+    //                     </ul>
+    //                 </div>`
+    //         $("#test-col").append(breadTest);
+    //     }
+    // })
 });
 
 // Cocktail DB
-$.ajax({
-    url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
-    type: "GET"
-}).done(function(data) {
-    console.log(data);
-});
+// $.ajax({
+//     url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita",
+//     type: "GET"
+// }).done(function(data) {
+//     console.log(data);
+// });
 
 // Mapbox
 
