376b68e80cb211d92090c83490b72c5868b3a3e8
Cierra Smith <cierra.r.smith3@gmail.com>
2021-11-10 19:22:42 -0600

    search recipes working

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -2,9 +2,10 @@
 // let requestString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + spoonacularKey + "&ingredients=apples,+flour,+sugar&number=2";
 
 //Spoonacular
-// let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
+let searchFood = "https://api.spoonacular.com/food/ingredients/search?apiKey="+spoonacularKey+"&number=10&query=";
 let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
 // let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
+
 
 $.ajax({
     url: requestString,
@@ -28,29 +29,19 @@ $.ajax({
     }
     $("#test-col").html(html);
 
-
-
-
-
-    // $("#test-col").html("");
-    // data.results.forEach(function (item, index) {
-    //     if (index < 3) {
-    //         console.log(item);
-    //
-    //         var breadTest = `
-    //                 <div class="column" id="test-col">
-    //                     <div class="card" style="width: 18rem;">
-    //                     <ul class="list-group list-group-flush">
-    //
-    //                         <img src="https://spoonacular.com/cdn/ingredients_100x100/${item.image}">
-    //                         <li class="list-group-item">Name: ${item.name}</li>
-    //
-    //                     </ul>
-    //                 </div>`
-    //         $("#test-col").append(breadTest);
-    //     }
-    // })
 });
+
+// Search Food
+
+$.ajax({
+    url: searchFood,
+    type: "GET"
+}).done(function(data) {
+    console.log(data);
+
+});
+
+
 
 // Cocktail DB
 // $.ajax({
