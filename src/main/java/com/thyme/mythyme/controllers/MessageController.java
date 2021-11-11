3463d0b6a8c793d1f05259531a8d09afc925ebec
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-11-10 21:53:24 -0600

    attempting to restructure messages. Need to figure out sharing

--- src/main/java/com/thyme/mythyme/controllers/MessageController.java
+++ src/main/java/com/thyme/mythyme/controllers/MessageController.java
@@ -2,6 +2,7 @@ package com.thyme.mythyme.controllers;
 
 import com.thyme.mythyme.models.Messages;
 import com.thyme.mythyme.models.User;
+import com.thyme.mythyme.repository.GroceryListRepository;
 import com.thyme.mythyme.repository.MessagesRepository;
 import com.thyme.mythyme.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,48 +20,42 @@ public class MessageController {
 
 
     private final MessagesRepository messageDao;
-
-
+    private final GroceryListRepository groceryDao;
     private final UserRepository userDao;
 
-    public MessageController(MessagesRepository messageDao, UserRepository userDao) {
+    public MessageController(MessagesRepository messageDao, GroceryListRepository groceryDao, UserRepository userDao) {
         this.messageDao = messageDao;
-
+        this.groceryDao = groceryDao;
         this.userDao = userDao;
     }
 
     @GetMapping("/messages/{otherUserId}")
     public String newMessage(Model model,  @PathVariable long otherUserId) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User user = userDao.getById(loggedInUser.getId());
+        User user = userDao.findByUsername(loggedInUser.getUsername());
         model.addAttribute("loggedinuser", user);
 
         model.addAttribute("userMessage", new Messages());
 
-//        List<Messages> messageList = user.getReceivedMessages();
-//        List<Messages> sentMessages = user.getSentMessages();
+        List<Messages> messageList = user.getReceivedMessages();
+        List<Messages> sentMessages = user.getSentMessages();
 
-//        sentMessages.addAll(messageList);
+        for (Messages message : sentMessages) {
+            messageList.add(message);
+        }
 
-//        Collections.sort(messageList);
+        Collections.sort(messageList);
 
        User otherUser = userDao.getById(otherUserId);
 
-        List<Messages> sentMessages = messageDao.getAllBySenderAndReceiver(loggedInUser, otherUser);
-        List<Messages> receivedMessages = messageDao.getAllBySenderAndReceiver(otherUser, loggedInUser);
-
-
         model.addAttribute("otheruser", otherUser);
-        model.addAttribute("sentmessages", sentMessages);
-        model.addAttribute("receivedmessages", receivedMessages);
+        model.addAttribute("messages", messageList);
 
-        return "user/message";
-
-
+        return "messages/message";
     }
 
     @PostMapping("/messages/{id}")
-    public String sendMessage(Model model, @PathVariable long id, @ModelAttribute Messages userMessage) {
+    public String sendMessage(Model model, @PathVariable long id, @ModelAttribute Messages message) {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.findByUsername(loggedInUser.getUsername());
         User receivingUser = userDao.getById(id);
@@ -67,7 +63,7 @@ public class MessageController {
         Messages newMessage = new Messages();
         newMessage.setSender(loggedInUser);
         newMessage.setReceiver(receivingUser);
-        newMessage.setContent(userMessage.getContent());
+        newMessage.setContent(message.getContent());
         newMessage.setTimestamp(new Timestamp(System.currentTimeMillis()));
         messageDao.save(newMessage);
 
@@ -77,12 +73,30 @@ public class MessageController {
 
     }
 
-//    @GetMapping("messages")
-//    public String messageLog(Model model){
-//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//
-//
-//    }
+    @GetMapping("messages")
+    public String messageLog(Model model){
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDao.findByUsername(loggedInUser.getUsername());
+
+        List<Messages> messageHistory = new ArrayList<>();
+        List<User> listOfUsers = new ArrayList<>();
+
+        messageHistory.addAll(user.getReceivedMessages());
+        messageHistory.addAll(user.getSentMessages());
+
+
+        for(int i = 0; i < messageHistory.size(); i++) {
+            if(!listOfUsers.contains(messageHistory.get(i).receiver) && user.getId() != messageHistory.get(i).receiver.getId()) {
+                listOfUsers.add(messageHistory.get(i).receiver);
+            } else if (!listOfUsers.contains(messageHistory.get(i).sender) && user.getId() != messageHistory.get(i).sender.getId()) {
+                listOfUsers.add(messageHistory.get(i).sender);
+            }
+        }
+
+        model.addAttribute("users", listOfUsers);
+
+        return "messages/index";
+    }
 
 //    ///////// Sharing
 //    @GetMapping("/groceryLists/share/{shareURL}")
@@ -92,7 +106,7 @@ public class MessageController {
 //        model.addAttribute("listToShare", listToShare);
 //        return "groceryList/share";
 //    }
-//
+
 //    @PostMapping("/groceryLists/share/{shareURL}")
 //    public String shareGroceryList (@PathVariable String shareURL, @ModelAttribute GroceryList SharedList){
 //
