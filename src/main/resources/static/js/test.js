43c1d80a5daca36fffb088579e9130cf15069107
Kamrun Foster <kamrun.foster4@gmail.com>
2021-11-12 23:05:47 -0600

    updated landing page index card layout template css

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -3,13 +3,9 @@
 //
 // //Spoonacular
 // // let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
-// let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
+let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
 // // let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
-//
-//Spoonacular
-// let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
-// let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
-// let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
+
 
 $.ajax({
     url: requestString,
@@ -21,13 +17,13 @@ $.ajax({
     for (let i = 0; i < 3; i++) {
 
         html += `
-                  <div class="card" style="width: 18rem"><!-- class="bg-transparent" -->
-                    <div class="card-body" style="background-color: #9DC88D">
-                      <h5 class="card-title text-light font-weight-bold" style="border: #164A41; text-align: center">${data.recipes[i].title}</h5>
-                      <img class="centerImg shadow bg-white rounded" src="${data.recipes[i].image}" style="width: 8rem; length: 8rem; border: #9DC88D 2px solid">
+                  <div class="card ml-4" style="width: 18rem"><!-- class="bg-transparent" -->
+                    <div class="card-body" style="display: flex; flex-direction: column; background-color: #9DC88D">
+                      <h5 class="card-title text-light font-weight-bold" style="display: flex; flex-direction: row; border: #164A41; text-align: center">${data.recipes[i].title}</h5>
+                      <img class="centerImg shadow bg-white rounded" src="${data.recipes[i].image}" style="display: flex; flex-direction: row; width: 8rem; length: 8rem; border: #9DC88D 2px solid">
                       <br>
-                      <p class="card-text scroll shadow p-3 mb-5 rounded" style="background-color: #ffffff; color: #164A41">${data.recipes[i].summary}</p>
-                      <a href="#" class="btn centerImg shadow p-3 mb-2 mbr-2 rounded" style="color: white; background-color: #f1b24a">View recipe</a>
+                      <p class="card-text scroll shadow p-3 mb-5 rounded" style="display: flex; flex-direction: row; background-color: #ffffff; color: #164A41">${data.recipes[i].summary}</p>
+                      <a href="#" class="btn centerImg shadow p-3 mb-2 mbr-2 rounded" style="width: 80%; color: white; background-color: #f1b24a">View recipe</a>
 
                     </div>
                   </div>`;
