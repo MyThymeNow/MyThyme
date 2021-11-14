9f56b0b27ecddb6b94dd6f6478dba8b9f760d422
Kamrun Foster <kamrun.foster4@gmail.com>
2021-11-14 09:23:17 -0600

    corrected certain issues in test.js

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,54 +1,39 @@
-// //
-// // // let requestString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + spoonacularKey + "&ingredients=apples,+flour,+sugar&number=2";
-// //
-// // //Spoonacular
-// // // let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
-// let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
-// // // let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
-//
-//
-<<<<<<< HEAD
+"use strict"
 // //Spoonacular
 // // let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
-//
 // //
 // // // let requestString = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + spoonacularKey + "&ingredients=apples,+flour,+sugar&number=2";
 // //
 // // //Spoonacular
 // // // let requestString = "https://api.spoonacular.com/food/ingredients/search?apiKey=" + spoonacularKey + "&query=bread&number=30";
-// let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
+let requestString = "https://api.spoonacular.com/recipes/random?apiKey=" + spoonacularKey + "&number=3";
 // // // let requestCategories = "https://api.spoonacular.com/food/ingredients/9266/information?apiKey=" + spoonacularKey + "&amount=1";
 //
 //
-=======
->>>>>>> 119d7c3e7b1754aeb3dc4f213557b54f7e8c9106
-// $.ajax({
-//     url: requestString,
-//     type: "GET"
-// }).done(function(data) {
-//     console.log(data);
-//
-//     let html = "";
-//     for (let i = 0; i < 3; i++) {
-//
-//         html += `
-<<<<<<< HEAD
-//                   <div class="card ml-3" style="width: 18rem"><!-- class="bg-transparent" -->
-=======
-//                   <div class="card ml-4" style="width: 18rem"><!-- class="bg-transparent" -->
->>>>>>> 119d7c3e7b1754aeb3dc4f213557b54f7e8c9106
-//                     <div class="card-body" style="display: flex; flex-direction: column; background-color: #f1b24a"><!--9DC88D light green-->
-//                       <h5 class="card-title text-light font-weight-bold" style="display: flex; flex-direction: row; border: #164A41; text-align: center">${data.recipes[i].title}</h5>
-//                       <img class="centerImg shadow bg-white rounded" src="${data.recipes[i].image}" style="display: flex; flex-direction: row; width: 8rem; length: 8rem; border: #164A41 2px solid">
-//                       <br>
-//                       <p class="card-text scroll shadow p-3 mb-5 rounded" style="display: flex; flex-direction: row; background-color: #ffffff; color: #000">${data.recipes[i].summary}</p>
-//                       <a href="#" class="btn centerImg shadow p-3 mb-2 mbr-2 rounded" style="width: 80%; color: white; background-color: #164A41">View recipe</a>
-//
-//                     </div>
-//                   </div>`;
-//
-//     }
-//     $("#test-col").html(html);
+$.ajax({
+    url: requestString,
+    type: "GET"
+}).done(function(data) {
+    console.log(data);
+
+    let html = "";
+    for (let i = 0; i < 3; i++) {
+
+        html += `
+                  <div class="card ml-3" style="width: 18rem"><!-- class="bg-transparent" -->
+                  <div class="card ml-4" style="width: 18rem"><!-- class="bg-transparent" -->
+                    <div class="card-body" style="display: flex; flex-direction: column; background-color: #f1b24a"><!--9DC88D light green-->
+                      <h5 class="card-title text-light font-weight-bold" style="display: flex; flex-direction: row; border: #164A41; text-align: center">${data.recipes[i].title}</h5>
+                      <img class="centerImg shadow bg-white rounded" src="${data.recipes[i].image}" style="display: flex; flex-direction: row; width: 8rem; length: 8rem; border: #164A41 2px solid">
+                      <br>
+                      <p class="card-text scroll shadow p-3 mb-5 rounded" style="display: flex; flex-direction: row; background-color: #ffffff; color: #000">${data.recipes[i].summary}</p>
+                      <a href="#" class="btn centerImg shadow p-3 mb-2 mbr-2 rounded" style="width: 80%; color: white; background-color: #164A41">View recipe</a>
+
+                    </div>
+                  </div>`;
+
+    }
+    $("#test-col").html(html);
 //
 //
 //
@@ -68,7 +53,7 @@
 //     //     }
 //     // })
 //
-// });
+});
 //
 // // Cocktail DB
 // // $.ajax({
