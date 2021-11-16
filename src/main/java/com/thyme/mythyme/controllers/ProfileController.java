f911629b87e86d4cdeadc86ff55506f68e10e46f
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-11-15 19:59:29 -0600

    actually fixed edit profile this time

--- src/main/java/com/thyme/mythyme/controllers/ProfileController.java
+++ src/main/java/com/thyme/mythyme/controllers/ProfileController.java
@@ -32,17 +32,26 @@ public class ProfileController {
       User loggedInUser = userDao.getById(id);
 
        model.addAttribute("loggedInUser",loggedInUser);
+       model.addAttribute("firstName",loggedInUser.getFirstName());
+       model.addAttribute("lastName",loggedInUser.getLastName());
+       model.addAttribute("username",loggedInUser.getUsername());
+       model.addAttribute("email",loggedInUser.getEmail());
+       model.addAttribute("bio",loggedInUser.getBio());
+
 
        return "user/edit";
    }
 
     @PostMapping("/profile/edit/{id}")
     public String editedProfile(@PathVariable Long id,
+                                Model model,
                                 @RequestParam(name = "firstName") String firstName,
                                 @RequestParam(name = "lastName") String lastName,
                                 @RequestParam(name = "username") String username,
                                 @RequestParam(name = "email") String email,
                                 @RequestParam(name = "bio") String bio){
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userToDisplay = userDao.getById(currentUser.getId());
         User userToUpdate = userDao.getById(id);
 
         userToUpdate.setId(userToUpdate.getId());
@@ -51,8 +60,9 @@ public class ProfileController {
         userToUpdate.setUsername(username);
         userToUpdate.setEmail(email);
         userToUpdate.setBio(bio);
-
         userDao.save(userToUpdate);
+
+        model.addAttribute("user", userToDisplay);
         return "redirect:/profile";
     }
 
